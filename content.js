e2cbfee7956391c1c9671d4aed98d671cfdd357b
JParramore <jerome.parramore@gmail.com>
2023-04-16 18:22:55 +0100

    initial commit

---
+++ content.js
@@ -0,0 +1,27 @@
+(function () {
+    const ARROW_KEYS = ['ArrowRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown'];
+    const VIDEO_SELECTOR = 'video';
+    const CONTROLS_CLASS_NAME = 'ytp-chrome-bottom';
+
+    const handler = (e) => {
+        // isTrusted === cloned event
+        if (!e.isTrusted || !ARROW_KEYS.includes(e.key)) {
+            return;
+        }
+
+        const video = document.querySelector(VIDEO_SELECTOR);
+        const controls = document.getElementsByClassName(CONTROLS_CLASS_NAME)[0];
+
+        if (!video || !controls) {
+            return;
+        }
+
+        if (controls.contains(document.activeElement)) {
+            e.preventDefault();
+            const clone = new KeyboardEvent(e.type, e);
+            video.dispatchEvent(clone);
+        }
+    };
+
+    document.addEventListener('keydown', handler, true);
+})();
